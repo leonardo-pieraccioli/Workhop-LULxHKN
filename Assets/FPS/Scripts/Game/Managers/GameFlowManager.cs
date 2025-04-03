8c732ddcd4a3692114124b2de1af748acff13fde
Leonardo Pieraccioli <leonardo.pieraccioli@gmail.com>
2025-04-04 00:57:20 +0200

    [UPDATE] Now die and win reload the current scene

--- Assets/FPS/Scripts/Game/Managers/GameFlowManager.cs
+++ Assets/FPS/Scripts/Game/Managers/GameFlowManager.cs
@@ -56,7 +56,7 @@ namespace Unity.FPS.Game
                 // See if it's time to load the end scene (after the delay)
                 if (Time.time >= m_TimeLoadEndGameScene)
                 {
-                    SceneManager.LoadScene(m_SceneToLoad);
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
                     GameIsEnding = false;
                 }
             }
